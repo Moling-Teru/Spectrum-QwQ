31795fde02a2414ec4252a69b71dbdb5da9710be
Moling-Teru <moling-teru@qq.com>
2025-07-19 13:32:35 +0800

    Remove un-Linux files (l.1)

--- soft_link.bat
+++
@@ -1,9 +0,0 @@
-chcp 65001
-set LOCATION=%1
-%1 start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
-::mkdir "C:\Windows\ffmpeg\bin"
-mklink "C:\Windows\ffmpeg.exe" "%LOCATION%\bin\ffmpeg.exe"
-mklink "C:\Windows\ffprobe.exe" "%LOCATION%\bin\ffprobe.exe"
-mklink "C:\Windows\ffplay.exe" "%LOCATION%\bin\ffplay.exe"
-echo 软链接创建完成
-pause
