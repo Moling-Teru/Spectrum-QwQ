247a26b96a27c47f7b8a2ab480345cae2381d244
Moling-Teru <moling-teru@qq.com>
2025-07-19 13:26:59 +0800

    Revise FFmpeg download (p.5.2)

--- soft_link.bat
+++ soft_link.bat
@@ -1,6 +1,7 @@
+@echo off
 chcp 65001
 set LOCATION=%1
-%1 start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+::%1 start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 ::mkdir "C:\Windows\ffmpeg\bin"
 mklink "C:\Windows\ffmpeg.exe" "%LOCATION%\bin\ffmpeg.exe"
 mklink "C:\Windows\ffprobe.exe" "%LOCATION%\bin\ffprobe.exe"
